47c3eae453755e354146e685d16d3403c6024eae
chhsiao93 <chhsiao@utexas.edu>
2025-02-13 15:14:39 -0600

    remove bo png

--- plot_bo_result.py
+++ plot_bo_result.py
@@ -1,67 +1,76 @@
-import imageio.v2 as imageio
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-from skopt.plots import plot_convergence, plot_gaussian_process
 from scipy import interpolate
+import argparse
 
-var_name = 'phi'
-loss_name = 'mse'
-acq_func = 'EI'
-output_name = 'test'
-var_bound = [(35.0, 55.0)]
-n_random_starts = 5 # initialize Gaussian prior with 5 random evaluations
-n_calls = 15 # total evaluation in Bayesian, including random start.
+#input arguments
+parser = argparse.ArgumentParser(description='Plot BO result')
+parser.add_argument('--var_name', type=str, choices=['phi', 'vel'], default='phi', help='variable name')
+parser.add_argument('--loss_name', type=str, choices=['mse', 'ssim', 'depth'], default='mse', help='loss name')
 
-# get true loss landscape
-loss_landscape = np.load(f'data/losslandscape_{var_name}.npz', allow_pickle=True)
-loss_x = loss_landscape[var_name]
-loss_y = loss_landscape[loss_name]
-if len(loss_y.shape) >1:
-    loss_y = np.mean(loss_y, axis=-1)
-assert loss_x.shape[0] == loss_y.shape[0]
-print(loss_x.shape, loss_y.shape)
+args = parser.parse_args()
+var_name = args.var_name
+loss_name = args.loss_name
+output_name = 'bo_' + var_name
+landscape = np.load(f'data/losslandscape_{var_name}.npz', allow_pickle=True)
+var_x = np.squeeze(landscape[var_name])
+loss = landscape[loss_name]
+if loss_name == 'ssim':
+    loss = -1.0 * loss
+loss = np.mean(loss,axis=1)
 
-# use loss landscape as true objective funciton
-interpolate_func = interpolate.interp1d(loss_x, loss_y)
+interpolate_func = interpolate.interp1d(var_x, loss)
 def f(x):
     y = interpolate_func(x[0])
     return y
 
 # reading BO result file
-with open(f'result/{output_name}.pkl', 'rb') as file:
+with open(f'result/{output_name}_{loss_name}.pkl', 'rb') as file:
     res = pickle.load(file)
-    
-print(f'Opimtized {var_name} is at {res.x}, and the estimate loss is {res.fun}')
-# plot
-ax = plot_convergence(res)
-plt.savefig(f'result/convergence_{var_name}_{loss_name}.png',dpi=200)
 
-for frame in range(n_calls-n_random_starts+1):
-    fig, ax = plt.subplots(1,2,figsize=(10,5))
-    # Update the data for the line plot
-    plot_gaussian_process(res, n_calls=frame,
-                               objective=f,
-                               noise_level=0,
-                               show_legend=True, show_title=False,
-                               show_next_point=True, show_acq_func=True, ax=ax[0])
-    plot_gaussian_process(res, n_calls=frame,
-                               show_legend=True, show_title=False,
-                               show_mu=False, show_acq_func=True,
-                               show_observations=False,
-                               show_next_point=True, ax=ax[1])
-    ax[0].set_title(f'Iteration: {frame}')
-    ax[0].set_xlabel(var_name)
-    ax[0].set_ylabel('Loss')
-    ax[0].set_xlim(var_bound[0][0],var_bound[0][1])
-    ax[1].set_xlabel(var_name)
-    ax[1].set_ylabel(acq_func)
-    ax[1].set_xlim(var_bound[0][0],var_bound[0][1])
-    plt.tight_layout()
-    plt.savefig(f'result/bo_{var_name}_{frame:02d}.png',dpi=200)
+print(f'Opimtized {var_name} is at {res.x}, and the estimate loss is {res.fun}')
+n_random_starts = res.specs['args']['n_random_starts']
+n_calls = res.specs['args']['n_calls']
+space = res.space
+x = np.linspace(space.bounds[0][0], space.bounds[0][1], 1000).reshape(-1, 1)
+
+for i in range(n_calls-n_random_starts+1):
+    # Extracting the Gaussian process model and the space
+    gp = res.models[i]
+    gp.fit(res.x_iters[:n_random_starts+i], res.func_vals[:n_random_starts+i])
+    # Predicting the mean and standard deviation of the GP at the grid points
+    y_pred, sigma = gp.predict(x, return_std=True)
+    # Plotting the GP mean and confidence interval
     
-# make png to gif
-images = []
-for frame in range(n_calls-n_random_starts+1):
-    images.append(imageio.imread(f'result/bo_{var_name}_{frame:02d}.png'))
-imageio.mimsave(f'result/bo_{var_name}_{loss_name}.gif', images, format='GIF', fps=2)
+
+    
+    if i == n_calls-n_random_starts: # the final plot
+        fig, ax = plt.subplots(1,1, figsize=(6, 5))
+        # Plot loss landscape
+        plt.plot(var_x, loss, 'r-', label='Loss Landscape')
+        plt.plot(x, y_pred, 'k-', label='GP mean')
+        
+        plt.fill_between(x.ravel(), y_pred - 1.96 * sigma, y_pred + 1.96 * sigma, alpha=0.2, label='95% confidence interval')
+        # # Plot the random initialization
+        plt.scatter(res.x_iters[:n_random_starts], res.func_vals[:n_random_starts], marker='^', edgecolor='k', facecolor='none', s=100, label='initial observations')
+        
+        obs_x = np.array(res.x_iters)[n_random_starts:n_random_starts+i,0]
+        obs_y = res.func_vals[n_random_starts:n_random_starts+i]
+        # plot the rest of the points with different color
+        plt.scatter(obs_x, obs_y, c=np.arange(len(obs_x)), cmap='viridis', s=50, label='BO observation')
+        # plot number on the points
+        # this won't work for eps file
+        for num in range(0, len(obs_x), 2):
+            plt.annotate(str(num+1), (obs_x[num], obs_y[num]), xytext=(1, 5), textcoords='offset points')
+        
+        plt.title('Bayesian Optimization')
+        plt.xlabel(var_name)
+        plt.ylabel(loss_name)
+        plt.xlim(space.bounds[0])
+        plt.legend()
+        plt.tight_layout()
+        # plt.savefig(f'result/bo_{var_name}_{loss_name}.png', dpi=200)
+        plt.savefig(f'result/bo_{var_name}_{loss_name}.pdf')
+        # plt.savefig(f'result/bo_{var_name}_{loss_name}.eps', format='eps')
+        plt.show()
--- plot_losslandscape.py
+++ plot_losslandscape.py
@@ -1,43 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-var_name = 'vel'
+# Assuming var_name is defined somewhere above this code
+var_name = 'phi'
+
 xlabel = 'actuator velocity'
 ground_truth = -0.2
 xmin, xmax = -0.3, 0.1
+if var_name == 'phi':
+    xlabel = 'friction angle (degree)'
+    ground_truth = 45.0
+    xmin, xmax = 35.0, 50.0
+
 data = np.load(f'data/losslandscape_{var_name}.npz', allow_pickle=True)
-# data = np.load('data/losslandscape_vel.npz', allow_pickle=True)
-x = np.squeeze(data[var_name]) #vel or phi
-mse = data['mse']
-ssim = -1.0*data['ssim'] # negative ssim
-depth = data['depth']
-mean_mse = np.mean(mse,axis=1)
-mean_ssim = np.mean(ssim,axis=1)
-mean_depth = np.mean(depth,axis=1)
-n_view = mse.shape[1]
-print(f'n view: {n_view}')
-fig, ax = plt.subplots(1,3,figsize=(18,6))
-ax[0].plot(x, mean_mse, 'k-', label='mean', lw=2)
-ax[1].plot(x, mean_ssim, 'k-', label='mean', lw=2)
-ax[2].plot(x, mean_depth, 'k-', label='mean', lw=2)
-for view in range(n_view):
-    ax[0].plot(x, mse[:,view], '--', label=f'view {view}')
-    ax[1].plot(x, ssim[:,view], '--', label=f'view {view}')
-    ax[2].plot(x, depth[:,view], '--', label=f'view {view}')
-ax[0].set_xlabel(xlabel, fontsize=15) #actuator velocity or friction angle (degree)
-ax[1].set_xlabel(xlabel, fontsize=15) 
-ax[2].set_xlabel(xlabel, fontsize=15) 
-ax[0].set_ylabel('MSE (RGB)', fontsize=15)
-ax[1].set_ylabel('Negative SSIM', fontsize=15)
-ax[2].set_ylabel('MSE (Depth)', fontsize=15)
-ax[0].set_xlim(xmin, xmax)
-ax[1].set_xlim(xmin, xmax)
-ax[2].set_xlim(xmin, xmax)
-ax[0].legend()
-ax[1].legend()
-ax[2].legend()
-ax[0].axvline(ground_truth, c='r', lw=2)
-ax[1].axvline(ground_truth, c='r', lw=2)
-ax[2].axvline(ground_truth, c='r', lw=2)
-plt.tight_layout()
-plt.savefig(f'{var_name}_losslandscape.png', dpi=200)
+x = np.squeeze(data[var_name]) # vel or phi
+
+colors = plt.get_cmap('tab20', 12)  # Get a colormap with 12 distinct colors
+
+for loss_name in ["mse", "ssim", "depth"]:
+    loss = data[loss_name]
+    n_view = loss.shape[1]
+    if loss_name == 'ssim':
+        loss = -1.0 * loss
+    mean_loss = np.mean(loss, axis=1)
+    fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+    ax.plot(x, mean_loss, 'k-', lw=2, label='mean')
+    ax.scatter(x[np.argmin(mean_loss)], np.min(mean_loss), c='k', s=30, alpha=0.8)  # best view
+    for view in range(n_view):
+        color = colors(view % 12)  # Cycle through the 12 colors
+        ax.plot(x, loss[:, view], '--', label=f'view {view}', color=color)  # plot each view
+        ax.scatter(x[np.argmin(loss[:, view])], np.min(loss[:, view]), color=color, s=30, alpha=0.8)  # best view
+
+    ax.axvline(ground_truth, c='r', lw=2, label='ground truth')
+    ax.set_xlabel(xlabel, fontsize=15)  # actuator velocity or friction angle (degree)
+    ax.set_ylabel(f'Loss ({loss_name})', fontsize=15)  # MSE or SSIM or Depth
+    ax.set_xlim(xmin, xmax)
+    # set up ymin for each loss
+    if loss_name == "mse":
+        ax.set_ylim(13e-5, 38e-5)
+    elif loss_name == "ssim":
+        ax.set_ylim(-0.73, -0.64)
+    elif loss_name == "depth":
+        ax.set_ylim(0.018, 0.05)
+
+    if loss_name == "mse":
+        # 3 cols of legends
+        ax.legend(loc='upper right', fontsize=10, ncol=4, columnspacing=0.5)
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(f'result/losslandscape_{var_name}_{loss_name}.eps', format='eps', dpi=300)  # save as eps
